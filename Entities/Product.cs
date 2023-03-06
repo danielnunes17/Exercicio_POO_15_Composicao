857514a09bf2da22559abc26e12e8ac2bb58a1dd
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-06 15:26:03 -0300

    A

---
+++ Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_POO_15_Composicao.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
